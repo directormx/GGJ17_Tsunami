fe4e86cb46e4d0097b04b6557ae961381f07f6f8
Uriel Carrillo <carrillouriel@gmail.com>
2017-01-22 11:15:22 -0700

    Se agrego la transicion de la ola al darle click al boton play

--- Assets/My Scripts/IntroController.cs
+++ Assets/My Scripts/IntroController.cs
@@ -3,14 +3,40 @@ using System.Collections;
 
 public class IntroController : MonoBehaviour {
 
-
+	public GameObject Ola;
+	GameObject olaSpawneada;
+	public float velocidadOla;
+	public float frecuencia;
 	void Start()
 	{
 		Time.timeScale = 1;
+		MostrarOla ();
 	}
 	public void IrALevel1()
 	{
 		Application.LoadLevel("Level1");
+	}
+
+	public void MostrarOla()
+	{
+		GameObject newGameObject = Instantiate(Ola);
+		newGameObject.transform.position = new Vector3(-15.177683f,-3.79f,0);
+		olaSpawneada = newGameObject;
+	}
+
+	public void EnviarOla()
+	{
+		InvokeRepeating ("AvanzarOla", frecuencia, frecuencia);
+	}
+
+	 void AvanzarOla()
+	{
+		if (olaSpawneada.transform.position.x <= 9.64) {
+			olaSpawneada.transform.Translate (new Vector3 (velocidadOla, 0, 0) * Time.deltaTime);
+		} else {
+			IrALevel1 ();
+		}
+
 	}
 
 }
--- Assets/My Scripts/TransportarObjetos.cs
+++ Assets/My Scripts/TransportarObjetos.cs
@@ -14,6 +14,7 @@ public class TransportarObjetos : MonoBehaviour {
 	void Update () {
 		MoverObjeto ();
 	}
+		
 
 	void MoverObjeto()
 	{
