c9e6197533d7917caf0188a707c19ae5a011d368
Uriel Carrillo <carrillouriel@gmail.com>
2017-01-21 21:16:45 -0700

    spawnea el player dead

--- Assets/My Scripts/OlaController.cs
+++ Assets/My Scripts/OlaController.cs
@@ -4,25 +4,35 @@ using System.Collections;
 public class OlaController : MonoBehaviour {
 
 	public GameObject canvas;
+	public GameObject playerDead;
 
 	public void OnCollisionEnter2D(Collision2D obj) {
 		if (obj.gameObject.name == "Player") {
-			Destroy (obj.gameObject);
 			MostrarGameOver ();
+			Destroy (obj.gameObject);
+
 		}
 
 	}
 
 	public void OnTriggerEnter2D(Collider2D node) {
 		if (node.gameObject.name == "Player") {
-			Destroy(node.gameObject);
 			MostrarGameOver ();
+			Destroy(node.gameObject);
+		}
 	}
-}
+
+	void SpawnPlayerDead()
+	{
+		GameObject newGameObject = Instantiate(playerDead);
+		newGameObject.transform.position =new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0);
+	}
+
 	public void MostrarGameOver()
 	{
 		canvas.SetActive (true);
-		StopTime ();
+		SpawnPlayerDead ();
+		//StopTime ();
 	}
 
 	void StopTime()
